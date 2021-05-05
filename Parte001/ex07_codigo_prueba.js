72ee7415ca214a9a7f193b5013317f4282d624a3
Pablo <pablovillarey@gmail.com>
2021-05-05 18:00:45 +0200

    Terminado la pregunta 007

--- Parte001/ex07_codigo_prueba.js
+++ Parte001/ex07_codigo_prueba.js
@@ -1,1 +1,15 @@
-console.log("Hola a");    
+console.log("JavaScript es tremendo!");
+
+function saludar(nombre){
+    console.log(`Hola ${nombre}`);
+}
+
+saludar("Prueba");
+
+
+setTimeout(() => {
+    console.log("Este texto se mostrará pasado 5 segundos");
+}, 5000);
+
+
+console.log("Fin!");

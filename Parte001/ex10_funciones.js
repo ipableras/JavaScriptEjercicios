065e65c4245818eba28f580888feace6d379e988
Pablo <pablovillarey@gmail.com>
2021-05-06 16:53:26 +0200

    Commit del ejercicio ex10_funciones.js

---
+++ Parte001/ex10_funciones.js
@@ -0,0 +1,26 @@
+// Ejercicio 10: Declarar una funcion usando la instruccion function.
+
+function sumar(a, b){
+    var suma = a +b;
+
+    // Retornar, devolverlas 
+    return suma;
+}
+
+console.log(sumar(2,4));
+
+
+function restar(a, b){
+    let resta = a - b
+
+    return resta;
+}
+
+console.log(restar(10, 5));
+
+
+function multiplicar(a, b){
+    return a*b;
+}
+
+console.log(multiplicar(4, 5));

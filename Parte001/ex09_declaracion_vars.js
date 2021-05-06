b8d0d78382136f11eea0bc5caf252b260b9d8577
Pablo <pablovillarey@gmail.com>
2021-05-06 13:47:30 +0200

    Commit de JavaScript ex09

---
+++ Parte001/ex09_declaracion_vars.js
@@ -0,0 +1,27 @@
+// Ejercicio 9: Crear variables con la sentencia var
+
+var numero;
+numero = 7;
+
+console.log(numero);
+
+// Parece que se puede poner asi
+console.log(typeof numero);
+// O así
+console.log(typeof(numero));
+
+numero = "7";
+console.log(numero);
+console.log(typeof(numero));
+
+var x, y;
+console.log(x, " - ", y);
+console.log(typeof x, " - ", typeof y);
+
+var a = 1, b = "JavaScript";
+
+console.log(a, " - " ,b);
+console.log(typeof a, " - ", typeof b);
+
+// window es un navegador
+// console.log(window.numero);

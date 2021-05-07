7af7dfb03b3d4857600035c71347894468e75a3d
Pablo <pablovillarey@gmail.com>
2021-05-07 18:42:36 +0200

    Commit ejercicio ex020_uso_operador_eqeqeq.js

---
+++ Parte001/ex020_uso_operador_eqeqeq.js
@@ -0,0 +1,13 @@
+// Ejercicio 20: Uso Básico del Operador de Igualdad Estricto
+
+// ===
+
+let numero = 5;
+
+console.log(numero == 5);
+console.log(numero == "5");
+// Ojo, en las === su que lo necesitan exactamente igual
+// Esta funciona
+console.log(numero === 5);
+// Esta no funciona
+console.log(numero === "5");

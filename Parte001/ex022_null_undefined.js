526b5ec88d77ba2fe530d816855f6069b20bb37a
Pablo <pablovillarey@gmail.com>
2021-05-08 21:01:48 +0200

    Commit ejercicio ex022_null_undefined.js

---
+++ Parte001/ex022_null_undefined.js
@@ -0,0 +1,20 @@
+// Ejercicio 22: Diferencia entre null y undefined
+
+let a;
+let b = null;
+
+console.log(typeof a === 'undefined');
+console.log(typeof a === undefined);
+console.log(a === undefined);
+console.log(a === 'undefined');
+
+console.log();
+
+console.log(a === null);
+
+console.log();
+
+console.log(typeof b === 'undefined');
+console.log(b === null);
+
+

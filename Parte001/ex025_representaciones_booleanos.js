3cfceb0c8a40a3ecc5b5cecbbdbef9b05484ba48
Pablo <pablovillarey@gmail.com>
2021-05-09 20:44:57 +0200

    Commit eje ex025_representaciones_booleanos.js

---
+++ Parte001/ex025_representaciones_booleanos.js
@@ -0,0 +1,105 @@
+// Ejercicio 25: Consultar Representaciones Válidas de Valores Lógicos
+
+if(0){
+    console.log("El valor de `0` es true");
+}else{
+    console.log("El valor de `0` es false");
+}
+
+if(-0){
+    console.log("El valor de `-0` es true");
+}else{
+    console.log("El valor de `-0` es false");
+}
+
+console.log();
+
+if(null){
+    console.log("El valor de `null` es true");
+}else{
+    console.log("El valor de `null ` es false");
+}
+
+console.log();
+
+if(false){
+    console.log("El valor de `false` es true");
+}else{
+    console.log("El valor de `false` es false");
+}
+
+console.log();
+
+if(NaN){
+    console.log("El valor de `NaN` es true");
+}else{
+    console.log("El valor de `NaN` es false");
+}
+
+console.log();
+
+if(undefined){
+    console.log("El valor de `undefined` es true");
+}else{
+    console.log("El valor de `undefined` es false");
+}
+
+console.log();
+
+if(""){
+    console.log('El valor de `""` es true');
+}else{
+    console.log('El valor de `""` es false');
+}
+
+
+console.log();
+
+if(1){
+    console.log("El valor de `1` es true");
+}else{
+    console.log("El valor de `1` es false");
+}
+
+console.log();
+
+if(3){
+    console.log("El valor de `3` es true");
+}else{
+    console.log("El valor de `3` es false");
+}
+
+if(-3){
+    console.log("El valor de `-3` es true");
+}else{
+    console.log("El valor de `-3` es false");
+}
+
+
+if(4){
+    console.log("El valor de `4` es true");
+}else{
+    console.log("El valor de `4` es false");
+}
+
+console.log();
+
+let objeto = {}
+
+
+if(objeto){
+    console.log("El valor de `objecto` es true");
+}else{
+    console.log("El valor de `objeto` es false");
+}
+
+console.log();
+
+let sumar = (a, b) => {return a + b};
+
+
+if(sumar){
+    console.log("El valor de `sumar` es true");
+}else{
+    console.log("El valor de `sumar` es false");
+}

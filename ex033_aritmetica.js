b3af7726ec54b46f73280e7bbcb05f36cb2af0af
Pablo <pablovillarey@gmail.com>
2021-05-11 19:12:53 +0200

    Commit ejercicio ex033_aritmetica.js

---
+++ ex033_aritmetica.js
@@ -0,0 +1,19 @@
+// Ejercicio 33: Aritmética Básica entre Objetos Date
+
+
+let t1 = Date.now();
+t1;
+console.log("Hola...");
+let t2 = Date.now();
+t2;
+
+// Para ver los tiempos, es mejor hacerlo en una terminal de Crome o
+// otra cualquiera, porque asi podemos ver los tiempo
+console.log(t2 - t1);
+
+
+
+
+
+
+

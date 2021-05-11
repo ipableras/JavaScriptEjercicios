71bec68edbf7be2fbf939869dc7a560f86a4da65
Pablo <pablovillarey@gmail.com>
2021-05-11 19:04:09 +0200

    Commit ejercicio ex032_fecha.js

---
+++ ex032_fecha.js
@@ -0,0 +1,21 @@
+// Ejercicio 32: Crear una Fecha por Medio del Objeto Date
+
+let hoy = new Date();
+
+console.log(hoy);
+
+
+let fecha = new Date("May 13, 2013 03:17:19");
+console.log(fecha);
+
+fecha = new Date(2013, 4, 13);
+console.log(fecha);
+
+fecha = new Date(2013, 4,13, 3, 17, 19);
+console.log(fecha);
+
+console.log(fecha.getMonth());
+console.log(fecha.getFullYear());
+console.log(fecha.getDate());
+console.log(fecha.getMilliseconds());
+

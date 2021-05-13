03b0eb6a73d536de3bcf0d392c6a1c3066be7bb7
Pablo <pablovillarey@gmail.com>
2021-05-13 19:04:36 +0200

    Commit ejercci ex047_diferencias_objeto_arreglo.js

---
+++ Parte001/ex047_diferencias_objeto_arreglo.js
@@ -0,0 +1,16 @@
+// Ejercicio 47: Averiguar la Diferente entre un Objeto y un Arreglo
+
+var lenguajes = ["JavaScript", "Pyton", "Java"];
+
+var persona = {nombre: "Pablo", apellido: "Juse", codigo: 12345};
+
+console.log(lenguajes);
+console.log(lenguajes[0]);
+
+console.log();
+
+console.log(persona);
+console.log(persona.nombre);
+
+//conso
+console.log(Object.keys(persona));

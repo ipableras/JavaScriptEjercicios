69eeb214bff88581987ca8c15ef1893826388b3b
Pablo <pablovillarey@gmail.com>
2021-05-13 18:42:09 +0200

    Commit ejercicio ex046_for_in_arreglo.js

---
+++ Parte001/ex046_for_in_arreglo.js
@@ -0,0 +1,16 @@
+// Ejercicio 46: Recorrer un Arreglo por medio del Ciclo for...in
+
+let animales = ['🐔', '🐷', '🐑', '🐇'];
+
+for(let i in animales){
+    console.log(i);
+};
+
+console.log();
+
+for(let i in animales){
+    console.log(animales[i]);
+};
+
+
+

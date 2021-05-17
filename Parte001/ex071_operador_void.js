335e033c3f3a3876b123c6c7ce1c9ccd7ccc7119
Pablo <pablovillarey@gmail.com>
2021-05-17 13:33:03 +0200

    Commit ejercici ex070_eliminar_elemento_arreglo.js

---
+++ Parte001/ex071_operador_void.js
@@ -0,0 +1,18 @@
+// Ejercicio 71: Uso del Operador void
+
+// undefined
+
+console.log(undefined);
+
+// Hay que hacerlo en un navegador
+//console.log(window.undefined);
+
+console.log(void(0));
+
+
+// No funciona en el VSC, hay que hacerlo en el navegador
+//console.log(void(document.body.style.backgroundColor="black"));
+
+
+
+

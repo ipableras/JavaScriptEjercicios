c76346f1dc1981d4ef7b123f152060fc0f3a21d1
Pablo <pablovillarey@gmail.com>
2021-05-17 12:40:37 +0200

    Commit ejercicio ex066_operador_suma_promocion.js

---
+++ Parte001/ex066_operador_suma_promocion.js
@@ -0,0 +1,39 @@
+//  Ejercicio 66: Uso del Operador de Suma para Promocionar un Objeto a un Número
+
+console.log(typeof "42");
+console.log("42");
+console.log(typeof +"42");
+console.log(+"42");
+console.log(+"1.4142");
+console.log(+"-1e5");
+
+console.log();
+
+let cadena = "2.7172";
+
+console.log(+cadena);
+
+console.log();
+
+// No es un número
+console.log(+{});
+
+// Si es un String
+console.log({});
+
+// No es un número
+console.log(+"JS");
+
+
+// Solo hace falta un numero +
+console.log(+ + + + "2");
+
+console.log(typeof Number("42"));
+console.log(Number("42"));
+
+console.log(typeof Number("JS"));
+
+// Esta es la forma más ráipo
+console.log(Number("JS"));
+
+

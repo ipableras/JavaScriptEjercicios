d5faa5e1e8cea5ba1362b34b618ad833e42e9d94
Pablo <pablovillarey@gmail.com>
2021-05-18 21:08:08 +0200

    Commit ejercicio ex074_operador_in.js

---
+++ Parte001/ex074_operador_in.js
@@ -0,0 +1,9 @@
+// Ejercicio 74: Comprobar si un Índice Está Presente en un Arreglo con in
+
+let lenguajes = ["JavaScript", "C#", "Pyton", "Java"];
+
+console.log(lenguajes.length);
+console.log(0 in lenguajes);
+console.log(3 in lenguajes);
+console.log(-1 in lenguajes);
+console.log(4 in lenguajes);

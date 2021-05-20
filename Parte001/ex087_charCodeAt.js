16dd5fae72a7ca3ceb648fd2146a15d25a83725e
Pablo <pablovillarey@gmail.com>
2021-05-20 13:47:01 +0200

    Muy poco interesante String

---
+++ Parte001/ex087_charCodeAt.js
@@ -0,0 +1,28 @@
+// Ejercicio 87: Uso de la Función charCodeAt
+
+let cadena = "A";
+
+console.log(cadena.charCodeAt(0));
+console.log(cadena.charCodeAt(1));
+
+console.log();
+
+cadena = "Ajax";
+console.log(cadena.charCodeAt(0));
+console.log(cadena.charCodeAt(1));
+
+console.log();
+
+cadema = "\u0041";
+console.log(cadena.charCodeAt(0));
+
+cadena = "\n";
+console.log(cadena.charCodeAt(0));
+
+cadena = "\t";
+console.log(cadena.charCodeAt(0));
+
+
+
+
+

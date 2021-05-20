e36e562315c34c024254add360d9d0d9f423e573
Pablo <pablovillarey@gmail.com>
2021-05-20 16:25:23 +0200

    Muy importante for(let i in texto)

---
+++ Parte001/ex088_interar_cadena_caracteres.js
@@ -0,0 +1,18 @@
+// Ejercicio 88: Iterar por Todos los Caracteres de una Cadena de Caracteres
+
+let texto = "¡JacaScript es génial!";
+
+for(let i in texto){
+    console.log("La posición actual es", i, "y el caracter es",texto[i]);
+}
+
+
+
+
+
+
+
+
+
+
+

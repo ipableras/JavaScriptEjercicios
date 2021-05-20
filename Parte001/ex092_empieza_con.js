fcc0eeddbcda2ec0ef3597c7fd649292432018db
Pablo <pablovillarey@gmail.com>
2021-05-20 17:11:33 +0200

    No muy importante String .startsWith

---
+++ Parte001/ex092_empieza_con.js
@@ -0,0 +1,18 @@
+// Ejercicio 92: Determinar si una Cadena de Caracteres Empieza con una Palabra Específica
+
+// startsWith
+
+let cadena = "Firefox es génial";
+
+console.log(cadena);
+console.log(cadena.startsWith("Firefox"));
+console.log(cadena.startsWith("firefox"));
+
+console.log();
+
+console.log(cadena.startsWith("Fire"));
+console.log(cadena.startsWith("Firefox "));
+
+console.log();
+
+console.log(cadena.startsWith("FirRfox"));

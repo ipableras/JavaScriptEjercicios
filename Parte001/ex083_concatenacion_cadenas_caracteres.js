f63a4af4af3af46f201a2bcaae14666350d41adb
Pablo <pablovillarey@gmail.com>
2021-05-20 11:31:02 +0200

    Commit  ex083_concatenacion_cadenas_caracteres.js

---
+++ Parte001/ex083_concatenacion_cadenas_caracteres.js
@@ -0,0 +1,37 @@
+// Ejercicio 83: Concatenar Cadenas de Caracteres con Literales de Plantilla
+
+let cadena1 = "JavaScript es ";
+let cadena2 = "tremendo";
+
+console.log(cadena1);
+console.log(cadena2);
+
+let frase = cadena1 + cadena2;
+
+console.log(frase);
+
+console.log();
+
+let lenguaje = "JavaScript";
+
+frase = `${lenguaje} es un lenguaje de programación`;
+
+console.log(frase);
+
+console.log();
+
+
+let nombre = "Pablo"
+let email = "pablo@mail.com"
+frase = `Su nombre es: ${nombre}. Su email: ${email}.`;
+console.log(frase);
+
+nombre = "Angela";
+email ="angela@mail.com";
+frase = `Su nombre es: ${nombre}. Su email: ${email}.`;
+console.log(frase);
+
+
+
+
+

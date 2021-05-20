d6842f91c24caecaed583cef670f4d9f8cfd3567
Pablo <pablovillarey@gmail.com>
2021-05-20 20:50:01 +0200

    Necesario String .replace()

---
+++ Parte001/ex097_reemplazar_cadenas.js
@@ -0,0 +1,14 @@
+// Ejercicio 97: Usar el Método replace para Reemplazar una Cadena de Caracteres
+
+let cadena = "JavaScript es génial";
+
+console.log(cadena);
+
+cadena = cadena.replace("Java", "ECMA");
+console.log(cadena);
+
+cadena = "JavaScript es génial. JavaScript es tremendo";
+
+cadena = cadena.replace("Java", "ECMA");
+console.log(cadena);
+

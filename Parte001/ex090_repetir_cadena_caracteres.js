5b865db22dab27f882806a61360b2b626c060a35
Pablo <pablovillarey@gmail.com>
2021-05-20 16:41:12 +0200

    poco importente String repeat()

---
+++ Parte001/ex090_repetir_cadena_caracteres.js
@@ -0,0 +1,16 @@
+// Ejercicio 90: Repetir una Cadena de Caracteres por Medio del Método repeat
+
+let cadena = "JavaSxript";
+
+console.log(cadena);
+console.log(cadena.repeat(3));
+
+console.log(typeof cadena.repeat(3));
+
+console.log(cadena.repeat(0));
+// console.log(cadena.repeat(-1)); // No pueden ser mayor de 0
+
+cadena = "\u0041";
+console.log(cadena.repeat(5));
+
+

6f63cc6f7e4eeeedc45db7a380de04985ad88055
Pablo <pablovillarey@gmail.com>
2021-05-20 20:18:25 +0200

    poco importante String .endsWith()

---
+++ Parte001/ex093_termina_con.js
@@ -0,0 +1,16 @@
+// Ejercicio 93: Determinar si una Cadena de Caracteres Termina con una Palabra Específica
+
+// JavaScript es tremendo
+
+let cadena = "JavaScript es tremendo";
+
+console.log(cadena);
+
+console.log(cadena.endsWith("tremendo"));
+console.log(cadena.endsWith("Tremendo"));
+
+console.log();
+
+console.log(cadena.endsWith("do"));
+
+

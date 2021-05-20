29f7d5e7578b88751d97db726e886f6072573e16
Pablo <pablovillarey@gmail.com>
2021-05-20 20:36:56 +0200

    Poco importante String

---
+++ Parte001/ex094_posicion_subcadena.js
@@ -0,0 +1,16 @@
+// Ejercicio 94: Encontrar la Posición en la que se Halla una Palabra
+
+let cadena = "La versión más reciente JavaScript es ES6";
+
+let indice = cadena.indexOf("JavaScript");
+
+console.log(indice);
+
+console.log();
+
+// Es distinto Mayot y menor 
+indice = cadena.indexOf("javascript");
+console.log(indice);
+
+
+
---
+++ Parte001/ex095_posicion_ultima_ocurrencia.js
@@ -0,0 +1,17 @@
+// Ejercicio 95: Encontrar la Última Posición de la Ocurrencia de una Palabra
+
+let cadena = "JavaScript es un lenguaje de programación. "+
+                "JavaScript se ejecuta en el navegadorLa versión más reciente de JavaScript es ES6";
+
+// indexOf
+
+console.log(cadena.indexOf("JavaScript"));
+
+console.log(cadena.lastIndexOf("JavaScript"));
+
+
+
+
+
+
+

0d5c17e75dd7e35a4f36a47b3fce2ac86fefc3ce
Pablo <pablovillarey@gmail.com>
2021-05-20 16:53:05 +0200

    importante String .indexOf()

---
+++ Parte001/ex091_existe_subcadena.js
@@ -0,0 +1,16 @@
+// Ejercicio 91: Comprobar si una Cadena de Caracteres Contiene una Subcadena
+
+// Solucion:
+
+// JavaScript es tremendo
+// Script
+
+let cadena = "JavaScript es tremendo";
+
+console.log(cadena);
+console.log(cadena.indexOf("Script"));
+console.log(cadena.indexOf("script"));
+console.log(cadena.indexOf("es"));
+
+
+

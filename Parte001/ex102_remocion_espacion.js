0042e53da25d9f41d74b44b9e6e3014e2fdbbef4
Pablo <pablovillarey@gmail.com>
2021-05-31 20:45:10 +0200

    normal String .trim()

---
+++ Parte001/ex102_remocion_espacion.js
@@ -0,0 +1,18 @@
+// Ejercicio 102: Remover el Espacio en Blanco de una Cadena de Caracteres con trim
+
+//         John Ortiz Ordoñez      
+
+let nombre = "         John Ortiz Ordoñez      ";
+console.log(nombre);
+
+nombre = nombre.trim();
+console.log(nombre);
+
+console.log();
+
+let cadena = "\nJavaScript\t ES6\n \t";
+console.log(cadena);
+
+cadena = cadena.trim();
+
+console.log(cadena);

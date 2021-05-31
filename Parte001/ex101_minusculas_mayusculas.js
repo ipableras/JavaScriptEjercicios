bc372c7f34f33a71a467b8dcd9f56aebad251a55
Pablo <pablovillarey@gmail.com>
2021-05-31 20:36:05 +0200

    No importante String .toLocaleLowerCase()

---
+++ Parte001/ex101_minusculas_mayusculas.js
@@ -0,0 +1,16 @@
+// Ejercicio 101: Alternar entre Minúsculas y Mayúsculas con toLowerCase y toUpperCase
+
+let cadena = "Vamos a Buscar algo Distinto";
+
+
+console.log(cadena);
+console.log(cadena.toLocaleLowerCase());
+console.log(cadena.toUpperCase());
+
+console.log();
+
+cadena = "\u0041";
+
+console.log(cadena);
+console.log(cadena.toLowerCase());
+console.log(cadena.toUpperCase());

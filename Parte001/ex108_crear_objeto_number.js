c6c66581d9ecb9ffa5b9ed554781a3ae73e37fb4
Pablo <pablovillarey@gmail.com>
2021-06-04 20:42:41 +0200

    Muy importante Objeto Number

---
+++ Parte001/ex108_crear_objeto_number.js
@@ -0,0 +1,24 @@
+// Ejercicio 108: Crear un Objeto de Tipo Number
+
+let numero = new Number(19);
+
+console.log(typeof numero);
+console.log(numero);
+
+console.log(numero instanceof Number);
+
+console.log();
+
+let primitivo = parseInt(numero.toString());
+
+console.log(typeof primitivo);
+console.log(primitivo);
+
+
+console.log();
+
+let suma = numero + primitivo;
+console.log(typeof suma);
+console.log(suma);
+
+

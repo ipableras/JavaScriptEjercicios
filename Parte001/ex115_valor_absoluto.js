e888e37c24d6943a051c353d31480f0dd6389a02
Pablo <pablovillarey@gmail.com>
2021-06-07 18:06:05 +0200

    No importante Math .abs()

---
+++ Parte001/ex115_valor_absoluto.js
@@ -0,0 +1,14 @@
+// Ejercicio 115: Calcular el Valor Absoluto de un Número con la Función Math.abs
+
+console.log(Math.abs(-1));
+console.log(Math.abs(1));
+
+console.log();
+
+console.log(Math.abs(null));
+console.log(Math.abs(undefined));
+console.log(Math.abs("-1"));
+console.log(Math.abs("1"));
+console.log(Math.abs(""));
+
+

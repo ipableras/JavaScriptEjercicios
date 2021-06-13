364ad24250bdde7c8009091bed4180fa55ca52e8
Pablo <pablovillarey@gmail.com>
2021-06-13 20:14:38 +0200

    Poco importante Number bits

---
+++ Parte001/ex123_construir_enteros_32_bits.js
@@ -0,0 +1,15 @@
+// Ejercicio 123: Crear Números Enteros de 32 bits con Operacioens a Nivel de Bits
+
+let numero = 2; // 10
+let resultado = numero << 4; // 100000
+console.log(resultado);
+
+console.log();
+
+resultado = numero >> 1; // 1
+console.log(resultado);
+
+resultado = numero << 7;
+console.log(resultado);
+
+

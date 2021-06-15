d513e8b946702f9f8411a783e4689c9bff0a769a
Pablo <pablovillarey@gmail.com>
2021-06-15 22:16:52 +0200

    Ejercicio importante Date() ISO8601

---
+++ Parte001/ex136_capturar_fecha.js
@@ -0,0 +1,12 @@
+// Ejercicio 136: Crear un Objeto Fecha a partir de una Fecha en Formato ISO 8601
+
+let cadenaISO8601 = "2021-06-16t22:14:47-00:00";
+let fecha = new Date(cadenaISO8601);
+
+console.log(fecha);
+
+
+
+
+
+

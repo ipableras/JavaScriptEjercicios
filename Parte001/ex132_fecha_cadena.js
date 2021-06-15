6502bb938334115f53e74b28002bfc828452884c
Pablo <pablovillarey@gmail.com>
2021-06-15 09:40:02 +0200

    Muy importante Object Date() Number

---
+++ Parte001/ex132_fecha_cadena.js
@@ -0,0 +1,14 @@
+// Ejercicio 132: Crear un Objeto Date con los Valores de Año, Mes y Día
+
+let agnio = 1994;
+let mes = 5;
+let dia = 21;
+
+let fechaNacimiento = new Date(agnio, mes, dia);
+
+console.log(typeof fechaNacimiento);
+console.log(fechaNacimiento instanceof Date);
+console.log(fechaNacimiento);
+
+
+

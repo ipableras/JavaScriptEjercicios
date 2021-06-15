db1542d5f1f1e1da62599ebd8bcdaa456c20f99f
Pablo <pablovillarey@gmail.com>
2021-06-15 09:45:15 +0200

    Muy importante Date.now()

---
+++ Parte001/ex133_uso_date_now.js
@@ -0,0 +1,11 @@
+// Ejercicio 133: Obtener la Fecha en Milisegundos a partir de la Función static Date.now
+
+let milisegundos = Date.now()
+
+console.log(typeof milisegundos);
+console.log(milisegundos);
+
+
+
+
+

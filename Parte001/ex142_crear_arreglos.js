7af24a23bcbf5fa9403918b0baa6e9e5edb6949a
Pablo <pablovillarey@gmail.com>
2021-06-16 19:13:36 +0200

    Muy importante Array con Literal o con Clase Array

---
+++ Parte001/ex142_crear_arreglos.js
@@ -0,0 +1,21 @@
+// Ejercicio 142: Crear Arreglos con una Literal o con la Clase Array
+
+// Solución:
+
+// Son dinamicos
+
+// Esta es la forma mas normal
+let numerosPares = [];
+// Esta es para cantidad los elementos
+let numerosImpares = new Array();
+
+console.log(typeof numerosPares);
+console.log(typeof numerosImpares);
+
+console.log(numerosPares instanceof Array);
+console.log(numerosImpares instanceof Array);
+
+// En realidad, significa lo mismo
+console.log(numerosPares);
+console.log(numerosImpares);
+

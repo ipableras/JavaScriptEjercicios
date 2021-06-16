a1ae2a10b741717d96c28eb5d354797ecdd0c750
Pablo <pablovillarey@gmail.com>
2021-06-16 19:26:51 +0200

    Myt importante Array length()

---
+++ Parte001/ex144_acceder_elementos_arreglo.js
@@ -0,0 +1,15 @@
+// Ejercicio 144: Acceder a los Elementos de un Arreglo
+
+let lenguajes = ["JavaScript", "Java", "C++", "C#"]
+
+console.log(lenguajes[0]);
+console.log(lenguajes[1]);
+console.log(lenguajes[2]);
+console.log(lenguajes[lenguajes.length-1]);
+
+console.log();
+
+// No existen
+console.log(lenguajes[-1]);
+console.log(lenguajes[5]);
+

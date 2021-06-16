73338987d50a10a790a0d708b9aa87914c8c5133
Pablo <pablovillarey@gmail.com>
2021-06-16 19:19:13 +0200

    Muy importante Array con Push

---
+++ Parte001/ex143_agregar_elementos_arreglo.js
@@ -0,0 +1,22 @@
+// Ejercicio 143: Agregar Elementos a un Arreglo con el Método push
+
+
+// Solución
+
+let lenguaje = ["Java", "JavaScript", "C++", "Pyton"];
+
+console.log(lenguaje);
+
+console.log(lenguaje.length);
+
+console.log();
+
+lenguaje.push("PHP");
+console.log(lenguaje);
+console.log(lenguaje.length);
+
+lenguaje.push("C");
+lenguaje.push("Kotlin");
+
+console.log(lenguaje);
+console.log(lenguaje.length);

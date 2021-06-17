b2aed174521500f3125fc02e9f48427a228c621f
Pablo <pablovillarey@gmail.com>
2021-06-17 13:55:58 +0200

    Muy importante Array length

---
+++ Parte001/ex147_eliminar_contenido_arreglo.js
@@ -0,0 +1,25 @@
+// Ejercicio 147: Eliminar Elementos de un Arreglo con la Propiedad length
+
+let numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros.length);
+console.log(numeros);
+
+console.log();
+
+numeros.length = 4;
+
+console.log(numeros.length);
+console.log(numeros);
+
+// Vamos a mirarlo de otra forma
+
+let letras = ["a", "b", "c", "d"];
+
+console.log(letras);
+console.log(letras.length);
+
+letras.length = 2;
+console.log(letras);
+console.log(letras.length);
+

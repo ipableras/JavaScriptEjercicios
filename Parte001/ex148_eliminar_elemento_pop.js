d390d0051829dcc2840ff0fc1836bf80a7d9441e
Pablo <pablovillarey@gmail.com>
2021-06-17 15:05:10 +0200

    Muy importante Array pop()

---
+++ Parte001/ex148_eliminar_elemento_pop.js
@@ -0,0 +1,37 @@
+// Ejercicio 148: Eliminar un Elemento de un Arreglo con el Método pop
+
+let numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros.length);
+console.log(numeros);
+
+console.log();
+
+let numeroEliminado = numeros.pop();
+
+console.log("Número eliminado: ");
+console.log("Cantidad de elementos del arreglo: ", numeros.length);
+console.log(numeros);
+console.log(numeroEliminado);
+
+console.log(numeros.pop());
+numeros.pop();
+numeros.pop();
+numeros.pop();
+
+console.log(numeros.pop());
+
+console.log();
+
+console.log(numeros);
+
+numeros.pop();
+
+console.log(numeros);
+
+numeros.pop();
+
+console.log(numeros.pop());
+
+console.log(numeros);
+numeros.pop();

7a07443c39620e588f3ed86bcf7afe93d9f9e066
Pablo <pablovillarey@gmail.com>
2021-06-17 12:03:17 +0200

    Muy importante Modificar el contenido Array

---
+++ Parte001/ex146_modificar_contenido_arreglo.js
@@ -0,0 +1,9 @@
+// Ejercicio 146: Modificar el Contenido de un Arreglo
+
+let lenguajes = ["JavaScript", "C#", "Java", "C++"];
+
+console.log(lenguajes);
+
+lenguajes[2] = "Java 8";
+console.log(lenguajes);
+

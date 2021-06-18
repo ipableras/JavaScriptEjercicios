63116d190d8b9bbd46c0879dc508b19ba5bd166e
Pablo <pablovillarey@gmail.com>
2021-06-18 21:14:54 +0200

    MUY IMPORTANTE ARRAY shift(), pop(), push()

---
+++ Parte001/ex149_eliminar_elemento_shift.js
@@ -0,0 +1,33 @@
+// Ejercicio 149: Eliminar el Primer Elemento de un Arreglo con shift
+
+let numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros.length);
+console.log(numeros);
+
+console.log();
+
+let numeroEliminado = numeros.shift();
+
+console.log(numeros.length);
+console.log("Número eliminado:");
+console.log(numeros);
+
+// Esto es mio
+// Recordando cosas del Array
+
+let pop = numeros.pop();
+
+console.log(numeros.length);
+console.log("Número eliminado:");
+console.log(numeros);
+
+console.log();
+
+let push = numeros.push(7);
+
+console.log(numeros.length);
+console.log("Número eliminado:");
+console.log(numeros);
+
+

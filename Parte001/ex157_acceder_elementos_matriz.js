881b262b43f92b49a41db0426470dcffddb78503
Pablo <pablovillarey@gmail.com>
2021-06-19 19:45:37 +0200

    Muy importante Array Bidimensional

---
+++ Parte001/ex157_acceder_elementos_matriz.js
@@ -0,0 +1,26 @@
+// Ejercicio 157: Acceder a los Elementos de una Matriz (Arreglo Bidimensional)
+
+let matriz = new Array();
+matriz.push([1, 2, 3]);
+matriz.push([4, 5, 6]);
+matriz.push([7, 8, 9]);
+
+console.log(matriz.length);
+console.log(matriz);
+console.log(typeof matriz);
+console.log(matriz instanceof Array);
+
+// Recordar
+console.log(matriz instanceof String);
+
+console.log();
+
+console.log(matriz[0]);
+console.log(matriz[1]);
+console.log(matriz[2]);
+
+// Obtener un elemento (valor) de la matriz:
+console.log(matriz[0][0]);
+console.log(matriz[2][2]);
+
+

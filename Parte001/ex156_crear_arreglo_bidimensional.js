5f2b0e99b29c202f6f163cbb246c042106c9af20
Pablo <pablovillarey@gmail.com>
2021-06-19 19:38:30 +0200

    Importante Arrays bidimensionales

---
+++ Parte001/ex156_crear_arreglo_bidimensional.js
@@ -0,0 +1,17 @@
+// Ejercicio 156: Crear un Arreglo Bidimensional (Matriz)
+
+
+let matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log(matriz.length);
+console.log(matriz);
+
+
+// Para verlo en mi cabeza, sería así:
+// [1, 2, 3]
+// [4, 5, 6]
+// [7, 8, 9]
+
+console.log();
+console.log(matriz[1]);
+

437e809a07f7b0b30eb723e5efc62b42e821ada6
Pablo <pablovillarey@gmail.com>
2021-06-21 20:00:42 +0200

    Muy importan igual como el ex155 borrar todo Array

---
+++ Parte001/ex160_eliminar_contenido_matriz.js
@@ -0,0 +1,25 @@
+// Ejercicio 160: Eliminar Todo el Contenido de una Matriz (Arreglo Bidimensional)
+
+let numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log(numeros.length);
+console.log(numeros);
+
+numeros = [];
+
+console.log(numeros.length);
+console.log(numeros);
+
+console.log();
+
+numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log(numeros.length);
+console.log(numeros);
+
+numeros.length = 0;
+
+console.log(numeros.length);
+console.log(numeros);
+
+

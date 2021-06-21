066c754f7b28d17d116eb70bf0a7420e2f2c85d7
Pablo <pablovillarey@gmail.com>
2021-06-21 20:03:26 +0200

    Muy importante, pasar de Array a String

---
+++ Parte001/ex161_matriz_cadena_caracteres.js
@@ -0,0 +1,12 @@
+// Ejercicio 161: Crear la Representación de Textual de una Matriz con toString
+
+let numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log(numeros.toString());
+
+
+
+
+
+
+

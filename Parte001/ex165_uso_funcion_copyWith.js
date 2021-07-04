5ea01a24f3780157faab4c0be57a45f2d943ef80
Pablo <pablovillarey@gmail.com>
2021-07-04 20:01:55 +0200

    Importante de Array .copyWithin()

---
+++ Parte001/ex165_uso_funcion_copyWith.js
@@ -0,0 +1,22 @@
+// Ejercicio 165: Uso de la Función copyWithin de la Clase Array
+
+let numeros = [1, 2, 3, 4, 5, 6];
+
+console.log(numeros);
+
+let resultado = numeros.copyWithin(3, 0)
+
+
+console.log(resultado);
+
+
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 9];
+console.log(numeros);
+resultado = numeros.copyWithin(3, 0)
+
+
+console.log(resultado);
+
+

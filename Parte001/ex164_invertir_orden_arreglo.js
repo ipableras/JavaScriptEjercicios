c65bdeabd6fa2f975e51485ccbf46a9acf35bdb1
Pablo <pablovillarey@gmail.com>
2021-07-04 19:51:03 +0200

    MUY IMPORTANTE Array .reverse()

---
+++ Parte001/ex164_invertir_orden_arreglo.js
@@ -0,0 +1,14 @@
+// Ejercicio 164: Invertir el Orden de los Elementos de un Arreglo con la Función reverse
+
+
+let numeros = [1, 2,3 ,4 ,5 ,6 ,7];
+
+console.log(numeros);
+
+let resultado = numeros.reverse();
+
+console.log(resultado);
+
+
+
+
